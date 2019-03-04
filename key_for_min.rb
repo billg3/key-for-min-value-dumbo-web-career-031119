ceab5583f252c1dd6a111bb656bbc60e72e958dd
Bill G <gbilly3@gmail.com>
2019-03-04 15:47:25 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,4 +3,15 @@
 
 def key_for_min_value(name_hash)
 
+  smallest_name = nil
+  smallest = 1000
+
+  name_hash.each do |key, value|
+  if value <= smallest
+    smallest = value
+    smallest_name = key
+
+  end
+end
+  smallest_name
 end
